60c5e80666ba51f047ab9630cb3983b811a15c9c
angeella <a.andreella8@gmail.com>
2024-07-02 08:30:40 +0200

    add r script

--- Lab03/lab03.R
+++ Lab03/lab03.R
@@ -1,15 +1,24 @@
+## ----setup, include=FALSE-------------------------------------------
 knitr::opts_chunk$set(echo = T, fig.align = "center", out.width = '80%', warning = F, message = F)
 rm(list = ls())
 #load("Lab02/lab02.RData")
 
+
+## -------------------------------------------------------------------
 load("../Lab02/lab02.RData")
 
+
+## -------------------------------------------------------------------
 library(igraph)
 library(ggraph)
 
+
+## -------------------------------------------------------------------
 par(mfrow = c(1,1))
 plot(net)
 
+
+## -------------------------------------------------------------------
 V(net)$name[which(B_net < quantile(B_net, .99))] = NA
 plot(net, 
      vertex.label=V(net)$name, 
@@ -19,6 +28,8 @@ plot(net,
      vertex.frame.color="#ffffff",
      main = "3 million Russian troll tweets")
 
+
+## -------------------------------------------------------------------
 set.seed(1234)
 plot(net, 
      vertex.size = log(igraph::betweenness(net)+1),
@@ -30,6 +41,8 @@ plot(net,
      vertex.frame.color="#ffffff",
      main = "3 million Russian troll tweets")
 
+
+## -------------------------------------------------------------------
 V(net)$label = V(net)$name
 V(net)$label[which(B_net < quantile(B_net, .99))] = NA
 V(net)$label.color = "black"
@@ -38,38 +51,56 @@ V(net)$color = as.numeric(factor(aType))
 E(net)$arrow.size<-.2 
 E(net)$edge.color<-"gray80"
 
+
+## -------------------------------------------------------------------
 set.seed(1234)
 plot(net)
 
+
+## -------------------------------------------------------------------
 llMDS = layout_with_mds(net)
 plot(net,layout= llMDS)
 
+
+## -------------------------------------------------------------------
 set.seed(1)
 llFR = layout_with_fr(net,niter = 5000)
 plot(net,layout= llFR)
 
+
+## -------------------------------------------------------------------
 par(mfrow=c(1,2), mar=c(0,0,0,0)) # plot four figures- 1 rows, 2 columns 
 plot(net, layout=layout_with_fr)
 plot(net, layout=llFR) 
 
+
+## -------------------------------------------------------------------
 par(mfrow= c(1,1))
 set.seed(1)
 llKK = layout_with_kk(net,dim = 2,maxiter = 100*vcount(net))
 plot(net,layout= llKK)
 
+
+## -------------------------------------------------------------------
 l <- layout_with_graphopt(net) 
 plot(net, layout=l)
 
+
+## -------------------------------------------------------------------
 l1 <- layout_with_graphopt(net, charge=0.001) 
 l2 <- layout_with_graphopt(net, charge=0.00000001) 
 par(mfrow=c(1,2), mar=c(1,1,1,1)) 
 plot(net, layout=l1) 
 plot(net, layout=l2)
 
+
+## -------------------------------------------------------------------
 par(mfrow=c(1,1))
 graph_attr(net,"layout")<-layout_with_lgl 
 plot(net)
 
+
+## -------------------------------------------------------------------
 layouts <- grep("layout_", ls("package:igraph"), value=TRUE)[-c(1:2)]
 
 layouts <- layouts[grepl("attr|graphopt|with_kk|with_fr|with_mds", layouts)] 
@@ -80,22 +111,16 @@ for(layout in layouts){
   plot(net, edge.arrow.mode=0, layout=l, main=layout) 
 }
 
+
+## -------------------------------------------------------------------
 V(net)$community <- gr$membership 
 colrs <- adjustcolor(sample(colors(distinct = TRUE),11), alpha=.6) 
 plot(net, 
      vertex.color=colrs[V(net)$community],
      vertex.label = NA)
 
-dist.from.Left <- igraph::distances(net, v=V(net)[aType=="Left"], to=V(net), weights=NA)
 
-oranges<-colorRampPalette(c("darkred", "gold"))
-col<-oranges(max(dist.from.Left)+1) 
-col<-col[dist.from.Left+1] 
-
-plot(net,
-     vertex.color=col,
-     vertex.label.color = "black")
-
+## -------------------------------------------------------------------
 right.path<-shortest_paths(net,
                         from=V(net)[aType=="Left"],
                         to=V(net)[aType=="Right"], 
@@ -110,8 +135,10 @@ vcol[unlist(right.path$vpath)]<-"gold" #color vertices
 plot(net,
      vertex.color=vcol,
      edge.color=ecol, 
-     edge.width=ew,edge.arrow.mode=0)
+     edge.width=ew,edge.arrow.mode=0, layout = layout_with_graphopt)
 
+
+## -------------------------------------------------------------------
 inc.edges <- incident(net, V(net)[aType=="Left"], mode="all") #mode argument ignored for undirected graphs
 ecol <- rep("gray80", ecount(net)) 
 ecol[inc.edges] <- "orange" 
@@ -119,28 +146,38 @@ vcol <- rep("grey40", vcount(net))
 vcol[V(net)[aType=="News"]] <- "gold" 
 plot(net, 
      vertex.color=vcol, 
-     edge.color=ecol)
+     edge.color=ecol, layout = layout_with_graphopt)
 
+
+## -------------------------------------------------------------------
 neigh.nodes <- neighbors(net, V(net)[aType=="Left"], mode="out")
 vcol[neigh.nodes] <- "#ff9d00" 
-plot(net, vertex.color=vcol)
+plot(net, vertex.color=vcol, layout = layout_with_graphopt)
 
+
+## -------------------------------------------------------------------
 par(mfrow=c(1,2)) 
-plot(net, mark.groups=which(membership(gr)==1), mark.col="#C5E5E7", mark.border=NA) 
+plot(net, mark.groups=which(membership(gr)==1), mark.col="#C5E5E7", mark.border=NA, layout = layout_with_graphopt)
 # Mark multiple groups: 
-plot(net, mark.groups=list(which(membership(gr)==1), which(membership(gr)==2)), mark.col=c("#C5E5E7","#ECD89A"), mark.border=NA)
+plot(net, mark.groups=list(which(membership(gr)==1), which(membership(gr)==2)), mark.col=c("#C5E5E7","#ECD89A"), mark.border=NA, layout = layout_with_graphopt)
 
+
+## ----eval = FALSE---------------------------------------------------
 ## #do not run!
 ## tkid <- tkplot(net) #tkid is the id of the tkplot that will open
 ## l <- tkplot.getcoords(tkid) # grab the coordinates from tkplot
 ## plot(net, layout=l)
 
+
+## -------------------------------------------------------------------
 library(ggraph) 
 ggraph(net) + 
   geom_edge_link(color = "gray70") + 
   geom_node_point(color = V(net)$color) + 
   theme_minimal()
 
+
+## -------------------------------------------------------------------
 library(tidygraph)
 df = as_tbl_graph(net)
 df %>% mutate(btw= .5*log(centrality_betweenness()+1)) %>%
@@ -152,11 +189,15 @@ ggraph(layout="fr") +
   theme_graph(base_family='sans') +
   theme(legend.position = "bottom")
 
+
+## -------------------------------------------------------------------
 groupL = as.character(membership(gr))
 table(groupL)
 groupL[groupL %in% c(10,7,9, 11, 4)] = 4
 groupL[groupL %in% c(8)] = 7
 
+
+## -------------------------------------------------------------------
 library(stringr)
 table(groupL, aType)
 groupL = str_replace(groupL, "1$", "Russian")
@@ -167,8 +208,12 @@ groupL = str_replace(groupL, "5", "Left")
 groupL = str_replace(groupL, "6", "Hashtager")
 groupL = str_replace(groupL, "7", "German")
 
+
+## -------------------------------------------------------------------
 assortativity(net, as.factor(groupL))
 
+
+## -------------------------------------------------------------------
 df = as_tbl_graph(net)
 df %>% mutate(btw= centrality_betweenness()) %>%
 ggraph(layout="fr") + 
@@ -180,6 +225,8 @@ ggraph(layout="fr") +
   scale_size(guide="none") +
   theme(legend.position = "bottom")
 
+
+## -------------------------------------------------------------------
 #load("../Lab02/lab02.RData")
 V(net)$better_gr = groupL
 
@@ -199,3 +246,4 @@ df %>%
   theme(legend.position = "bottom") +
   scale_size(guide="none") + 
   scale_edge_color_discrete(guide="none")
+
--- Lab04/lab04.R
+++ Lab04/lab04.R
@@ -1,84 +1,135 @@
+## ----setup, include=FALSE-------------------------------------------
 knitr::opts_chunk$set(echo = T, fig.align = "center", out.width = '80%', warning = F, message = F)
 
+
+## -------------------------------------------------------------------
 rm(list = ls())
 library(statnet)
-library(sand) #to load the data
+library(sand) #to load the lazega data
 library(ergm)
 data(lazega)
 
+
+## -------------------------------------------------------------------
 library(intergraph)
 lazega.ergm <- asNetwork(lazega)
 lazega.ergm
 
-years <- lazega.ergm %v% 'Years' # %v% references vertex attributes, equivalent to get.vertex.attribute(lazega.ergm, "Years")
+
+## -------------------------------------------------------------------
+v.names <- vertex_attr_names(lazega)
+sapply(v.names, function(x) table(vertex_attr(lazega, x)))
+
+
+## -------------------------------------------------------------------
+years <- lazega.ergm %v% 'Years' 
+# %v% references vertex attributes, 
+#equivalent to get.vertex.attribute(lazega.ergm, "Years")
+#from igraph.
 
 plot(lazega.ergm, 
      vertex.col = "tomato", 
      vertex.cex = years/10)
 
+
+## ----eval = FALSE---------------------------------------------------
 ## ?ergm.terms
 
-summary(lazega.ergm ~ edges) # Calculate the edges statistic for this network
 
-random_graph <- ergm(lazega.ergm ~ edges, control = control.ergm(seed = 1234))
+## -------------------------------------------------------------------
+summary(lazega.ergm ~ edges) 
 
+
+## -------------------------------------------------------------------
+random_graph <- ergm(lazega.ergm ~ edges)
+
+
+## -------------------------------------------------------------------
+theta <- coef(random_graph)
+theta
+
+
+## -------------------------------------------------------------------
 inv.logit <- function(logit){
   odds <- exp(logit)
   prob <- odds / (1 + odds)
   return(prob)
 }
 
-theta <- coef(random_graph)
 inv.logit(theta)
 
+
+## -------------------------------------------------------------------
 network.density(lazega.ergm)
 
+
+## -------------------------------------------------------------------
 summary(random_graph)
 
+
+## -------------------------------------------------------------------
 set.seed(1234)
 hundred_simulations <- simulate(random_graph, 
                   coef = theta,
                   nsim = 100,
-                  control = control.simulate.ergm(MCMC.burnin = 1000,
-                                             MCMC.interval = 1000))
+                  control = control.simulate.ergm(MCMC.burnin = 1000, #number of proposals before any MCMC sampling is done.
+                                                  MCMC.interval = 1000)) #Number of proposals between sampled statistics
 
-par(mfrow = c(3, 3))
-sapply(hundred_simulations[1:9], plot, vertex.cex = 1, vertex.col = "tomato")
 
+## -------------------------------------------------------------------
+par(mfrow = c(1, 3))
+sapply(hundred_simulations[1:3], plot, vertex.cex = 3, vertex.col = "tomato")
+
+
+## -------------------------------------------------------------------
 net_densities <- unlist(lapply(hundred_simulations, network.density))
 
 hist(net_densities, xlab = "Density", main = "", col = "lightgray")
 abline(v = network.density(lazega.ergm), col = "red", lwd = 3, lty = 2)
 abline(v = mean(net_densities), col = "blue", lwd = 3, lty = 1)
 
-gof_stats <- gof(random_graph)
 
-par(mfrow = c(2, 3))
+## -------------------------------------------------------------------
+gof_stats <- gof(random_graph,coef = theta)
+
+par(mfrow = c(2, 2))
 plot(gof_stats, main = '')
 
-summary(lazega.ergm~edges+triangle) # Look at the g(y) stats for this model
 
+## -------------------------------------------------------------------
+summary(lazega.ergm~edges+triangle) # 
+
+
+## ----eval = FALSE---------------------------------------------------
 ## tr_graph <- ergm(lazega.ergm ~ edges + triangles)
 
+
+## -------------------------------------------------------------------
 summary(lazega.ergm ~ edges + gwesp(log(3), fixed=TRUE)
- + nodecov("Seniority")
  + nodecov("Practice")
  + nodematch("Practice")
  + nodematch("Gender")
  + nodematch("Office"))
 
+
+## -------------------------------------------------------------------
 lazega.ergm.fit <- ergm(lazega.ergm ~ edges + gwesp(log(3), fixed=TRUE)
- + nodecov("Seniority")
  + nodecov("Practice")
  + nodematch("Practice")
  + nodematch("Gender")
  + nodematch("Office"))
-summary(lazega.ergm.fit)
 
+
+## -------------------------------------------------------------------
 anova(lazega.ergm.fit)
 
+
+## -------------------------------------------------------------------
 summary(lazega.ergm.fit)
 
+
+## -------------------------------------------------------------------
 gof.lazega.ergm <- gof(lazega.ergm.fit)
-par(mfrow=c(1, 3))
+par(mfrow=c(2, 2))
 plot(gof.lazega.ergm)
+
