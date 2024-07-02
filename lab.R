c46311614f805296892b581148fea5928710d8f8
angeella <a.andreella8@gmail.com>
2024-07-02 08:28:31 +0200

    last update

---
+++ lab.R
@@ -0,0 +1,12 @@
+
+# Load one network data from the network data package
+
+#Details: https://www.r-bloggers.com/2019/12/a-large-repository-of-networkdata/
+
+#1. Do some proper preprocessing
+
+#2. Compute some descriptive statistics including a clustering approach
+
+#3. Represent the network in a proper way
+
+#4. Fit an ERGM
